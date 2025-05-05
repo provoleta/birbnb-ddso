fcb4c5ef2823caa411c9da469399064d084a2097
provoleta <buhlertomas@gmail.com>
2025-05-05 15:53:01 -0300

    Arreglo Eslint y cambiamos el formateador a Prettier

--- BirBnB/exceptions/disponibilidadException.js
+++ BirBnB/exceptions/disponibilidadException.js
@@ -1,4 +1,4 @@
-export class DispoibilidadException extends Error {
+export class DisponibilidadException extends Error {
     constructor(alojamiento) {
         super()
         this.message = `El alojamiento ${alojamiento.nombre} no se encuentra disponible para la fecha solicitada.`
--- BirBnB/services/reserva-service.js
+++ BirBnB/services/reserva-service.js
@@ -1,8 +1,10 @@
 import ExcededTimeException from "../exceptions/excededTimeException"
-import DispoibilidadException from "../exceptions/disponibilidadException"
+import DisponibilidadException from "../exceptions/disponibilidadException"
+import dayjs from 'dayjs'
+
 
 export class reservaService {
-  constructor (reservaRepository) {
+  constructor(reservaRepository) {
     this.reservaRepository = reservaRepository
 
   }
@@ -10,14 +12,14 @@ export class reservaService {
   async update(reserva) {
     const reservaAmodificar = await this.reservaRepository.findById(reserva.id)
     if (!reservaAmodificar) return { error: 'not found' }
-    
+
     // verificar que la fecha buscada no coincida con una en la que no haya disponibilidad
 
-    alojamiento = reservaAmodificar.alojamiento
-    disponibilidad = alojamiento.estasDisponibleEn(reserva.rangoFechas)
+    const alojamiento = reservaAmodificar.alojamiento
+    const disponibilidad = alojamiento.estasDisponibleEn(reserva.rangoFechas)
 
-    if (!dispobibilidad) throw new DispoibilidadException(reserva.alojamiento)//return { message: 'El alojamiento no esta disponible en el rango de fechas solicitado. ' }
-    
+    if (!disponibilidad) throw new DisponibilidadException(reserva.alojamiento)//return { message: 'El alojamiento no esta disponible en el rango de fechas solicitado. ' }
+
     reservaAmodificar.rangoFechas = reserva.rangoFechas
 
     const reservaModificada = await this.reservaRepository.update(reservaAmodificar)
@@ -25,43 +27,43 @@ export class reservaService {
     return this.toDTO(reservaModificada)
   }
 
-// eliminacion de la reserva pedida, hay que ver lo de reservaId 
+  // eliminacion de la reserva pedida, hay que ver lo de reservaId 
   async delete(reservaId) {
-    reservaAeliminar = await this.reservaRepository.findById(reservaId)
-    
+    const reservaAeliminar = await this.reservaRepository.findById(reservaId)
+
     if (dayjs() > reservaAeliminar.rangofechas.fechaInicio) {
       //return { message: 'No se puede cancelar la reserva ya que la misma se encuentra en curso.'}
-      throw new ExcededTimeException(reserva)
+      throw new ExcededTimeException(reservaAeliminar)
     }
-    else { return await this.reservaRepository.delete(reserva.id) }
+    return await this.reservaRepository.delete(reservaId)
   }
 
   async create(reserva) {
     // verificar disponibilidad para el rango de fechas elegido
-    alojamiento = reserva.alojamiento
-    disponibilidad = alojamiento.estasDisponibleEn(reserva.rangoFechas)
+    const alojamiento = reserva.alojamiento
+    const disponibilidad = alojamiento.estasDisponibleEn(reserva.rangoFechas)
 
-    if(!disponibilidad) return {message: 'La fecha solicitada se encuentra ocupada'}
-    
-    reservaCreada = await this.reservaRepository.create(reserva)
+    if (!disponibilidad) return { message: 'La fecha solicitada se encuentra ocupada' }
+
+    const reservaCreada = await this.reservaRepository.create(reserva)
 
     return this.toDTO(reservaCreada)
   }
 
   async findByUserId(userId) {
-    reservas = await this.reservaRepository.filterById(userId)
+    const reservas = await this.reservaRepository.filterById(userId)
 
     if (!reservas) return { message: 'no se encontraron reservas del usuario' }
-    
-    historialReservas = reservas.map(reserva => this.toDTO(reserva))
+
+    const historialReservas = reservas.map(reserva => this.toDTO(reserva))
 
     return historialReservas
   }
 
-  toDTO (reserva) {
+  toDTO(reserva) {
     return {
       fechaAlta: reserva.fechaAlta,
-      huespedReservador:  reserva.huespedReservador,
+      huespedReservador: reserva.huespedReservador,
       alojamiento: reserva.alojamiento,
       rangoFechas: reserva.rangoFechas,
       estado: reserva.EstadoReserva,
