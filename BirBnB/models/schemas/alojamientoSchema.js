6eefe88a678aca03fa7de726d5d3ae837180ffba
Jere1203 <jbartolsic@frba.utn.edu.ar>
2025-05-10 13:38:32 -0300

    Arme schema para alojamiento

---
+++ BirBnB/models/schemas/alojamientoSchema.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose'
+import { Alojamiento, Caracteristica, Moneda, Foto } from '../entities/alojamiento.js'
+import Reserva from '../entities/reserva.js'
+
+const alojamientoSchema = new mongoose.Schema({
+  anfitrion: {
+    type: Usuario,
+    required: true,
+  },
+  nombre: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  precioPorNoche: {
+    type: Double,
+    required: true,
+  },
+  moneda: {
+    type: Moneda,
+    required: true,
+  },
+  horarioCheckin: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  horarioCheckOut: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  direcion: {
+    type: Direccion,
+    required: true,
+  },
+  cantHuespedesMax: {
+    type: Number,
+    required: true,
+  },
+  caracteristicas: {
+    type: Caracteristica,
+    required: true,
+  },
+  reservas: {
+    type: [Reserva],
+    required: true,
+  },
+  fotos: {
+    type: Foto,
+    required: true,
+  },
+})
+
+alojamientoSchema.loadClass(Alojamiento)
+
+export const alojamientoModel = mongoose.model('Alojamiento', alojamientoSchema)
