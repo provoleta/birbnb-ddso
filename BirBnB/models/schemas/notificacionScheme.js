11d44bf2e8d07efd3d3bd1f235152336fb04e406
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2025-05-10 13:00:33 -0300

    Agrego notificacionScheme

--- BirBnB/models/schemas/notificacionScheme.js
+++ BirBnB/models/schemas/notificacionScheme.js
@@ -1,2 +1,24 @@
 import mongoose from 'mongoose'
 import Notificacion from '../entities/notificacion.js'
+import { Usuario } from '../entities/usuario.js'
+import { Dayjs } from 'dayjs'
+
+const notificacionScheme = new mongoose.Schema({
+  mensaje: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  usuario: {
+    type: Usuario,
+    requiered: true,
+  },
+  fechaAlta: {
+    type: Dayjs,
+    requiered: true,
+  },
+})
+
+notificacionScheme.loadClass(Notificacion)
+
+export const NotificacionModel = mongoose.model('Notificacion', notificacionScheme)
