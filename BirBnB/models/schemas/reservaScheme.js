7f475ed36d5347035030bf6ea3f240735deb1d36
Nicolas <naguero@frba.utn.edu.ar>
2025-05-10 13:12:43 -0300

    schema de reserva hecho

--- BirBnB/models/schemas/reservaScheme.js
+++ BirBnB/models/schemas/reservaScheme.js
@@ -1,2 +1,39 @@
 import mongoose from 'mongoose'
-import { Reserva } from '../entities/reserva.js'
+import { Reserva, RangoFechas } from '../entities/reserva.js'
+import { Dayjs } from 'dayjs'
+
+const reservaSchema = new mongoose.Schema(
+  {
+    fechaAlta: {
+      type: Dayjs,
+      required: true,
+    },
+
+    usuario: {
+      type: mongoose.Schema.Types.Objectid,
+      ref: 'Usuario',
+    },
+
+    alojamiento: {
+      type: mongoose.Schema.Types.Objectid,
+      ref: 'Alojamiento',
+    },
+
+    rangoFechas: {
+      type: RangoFechas,
+      required: true,
+    },
+
+    precioPorNoche: {
+      type: Double,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+reservaSchema.loadClass(Reserva)
+
+export const ReservaModel = mongoose.model('Reserva', reservaSchema)
