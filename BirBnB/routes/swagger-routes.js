601d6bb8260bf807eed015e303fed2c52e46faa7
LucasArias <lucaarias@frba.utn.edu.ar>
2025-05-21 14:40:43 -0300

    Emprolijo el swagger

--- BirBnB/routes/swagger-routes.js
+++ BirBnB/routes/swagger-routes.js
@@ -5,7 +5,18 @@ const swaggerDocument = JSON.parse(
   await readFile(new URL('../../docs/api-docs.json', import.meta.url)),
 )
 
+const options = {
+  swaggerOptions: {
+    tryItOutEnabled: true,
+    defaultModelsExpandDepth: 5,
+    defaultModelExpandDepth: 5,
+  },
+  customCss:
+    '.swagger-ui .model-box { display: inline-block; } .swagger-ui .model-toggle { display: none !important; }',
+  customSiteTitle: 'BirBnB API Docs',
+}
+
 export default function registerSwaggerRoutes(app) {
   app.use('/api-docs', swaggerUiExpress.serve)
-  app.get('/api-docs', swaggerUiExpress.setup(swaggerDocument))
+  app.get('/api-docs', swaggerUiExpress.setup(swaggerDocument, options))
 }
