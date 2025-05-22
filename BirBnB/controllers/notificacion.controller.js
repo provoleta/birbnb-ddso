e852f3469325f7ea4898710ee8826dd44e608be2
zBlooDy <tperezgiacchetta@frba.utn.edu.ar>
2025-05-22 09:17:51 -0300

    Faltaba hacer dos validaciones del userId en el controller de notificacion

--- BirBnB/controllers/notificacion.controller.js
+++ BirBnB/controllers/notificacion.controller.js
@@ -9,6 +9,7 @@ export default class NotificacionController {
     const { userId, leida } = req.query
     const leidaBool = leida === 'true'
     try {
+      validarObjectId(userId)
       const notificaciones = await this.notificacionService.findAll(leidaBool, userId)
       return res.status(200).json(notificaciones)
     } catch (error) {
@@ -21,6 +22,7 @@ export default class NotificacionController {
 
     try {
       validarObjectId(id)
+      validarObjectId(userId)
       const notificacion = await this.notificacionService.markAsRead(id, userId)
       return res.status(200).json(notificacion)
     } catch (error) {
