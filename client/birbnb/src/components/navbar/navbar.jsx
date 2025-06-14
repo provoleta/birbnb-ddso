46b876b2487de7d4e03561a6c9e6b1983d56e617
LucasArias <lucaarias@frba.utn.edu.ar>
2025-06-14 08:56:15 -0300

    No se puede reservar para menos de 1 huesped

--- client/birbnb/src/components/navbar/navbar.jsx
+++ client/birbnb/src/components/navbar/navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           <input className="main-input left" type="text" placeholder="Buscar" />
           <input className="main-input" type="date" placeholder="Check-in" />
           <input className="main-input" type="date" placeholder="Check-out" />
-          <input className="main-input right" type="number" placeholder="Huespedes" />
+          <input className="main-input right" type="number" placeholder="Huespedes" min="1" />
           <button className="button-busqueda">Buscar</button>
         </div>
       </div>
