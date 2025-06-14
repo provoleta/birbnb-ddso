eb7b2aff0ea2323e714d80ceefcc5e59223193c2
Nicolas <naguero@frba.utn.edu.ar>
2025-06-14 17:38:34 -0300

    componentes para caracteristicas

---
+++ client/birbnb/src/components/caracteristica/caracteristica-item.jsx
@@ -0,0 +1,30 @@
+import WifiIcon from '@mui/icons-material/Wifi'
+import PoolIcon from '@mui/icons-material/Pool'
+import LocalParkingIcon from '@mui/icons-material/LocalParking'
+import PetsIcon from '@mui/icons-material/Pets'
+
+const CaracteristicaItem = ({ caracteristica }) => {
+  const getIcon = (caracteristica) => {
+    switch (caracteristica) {
+      case 'WIFI':
+        return <WifiIcon />
+      case 'PISCINA':
+        return <PoolIcon />
+      case 'ESTACIONAMIENTO':
+        return <LocalParkingIcon />
+      case 'MASCOTAS_PERMITIDAS':
+        return <PetsIcon />
+      default:
+        return null
+    }
+  }
+
+  return (
+    <div className="caracteristica-item">
+      {getIcon(caracteristica)}
+      <p>{caracteristica}</p>
+    </div>
+  )
+}
+
+export default CaracteristicaItem
