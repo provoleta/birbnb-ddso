3231f24c0131f045326321d854b0218e2f43c406
Nicolas <naguero@frba.utn.edu.ar>
2025-06-16 00:56:29 -0300

    agrego gran parte del css de detail-page 1 am che

--- client/birbnb/src/components/caracteristica/caracteristica-item.jsx
+++ client/birbnb/src/components/caracteristica/caracteristica-item.jsx
@@ -2,6 +2,7 @@ import WifiIcon from '@mui/icons-material/Wifi'
 import PoolIcon from '@mui/icons-material/Pool'
 import LocalParkingIcon from '@mui/icons-material/LocalParking'
 import PetsIcon from '@mui/icons-material/Pets'
+import '../../features/detail-page/detail-page.css'
 
 const CaracteristicaItem = ({ caracteristica }) => {
   const getIcon = (caracteristica) => {
@@ -19,12 +20,21 @@ const CaracteristicaItem = ({ caracteristica }) => {
     }
   }
 
+  function formatearPalabra(palabra) {
+    // Reemplaza _ por espacio, pone todo en minúscula
+    let sinGuion = palabra.replace(/_/g, ' ').toLowerCase()
+    // Capitaliza la primera letra
+    return sinGuion.charAt(0).toUpperCase() + sinGuion.slice(1)
+  }
+
   return (
     <div className="caracteristica-item">
-      <h3>
-        {getIcon(caracteristica)}
-        {caracteristica}
-      </h3>
+      <p>
+        {getIcon && (
+          <span className="icono-caracteristica">{getIcon(caracteristica)}</span>
+        )}
+        {formatearPalabra(caracteristica)}
+      </p>
     </div>
   )
 }
--- client/birbnb/src/features/detail-page/detail-page.jsx
+++ client/birbnb/src/features/detail-page/detail-page.jsx
@@ -3,6 +3,9 @@ import { Card } from '@mui/material'
 import './detail-page.css'
 import { alojamientos } from './alojamientosMockeados.js'
 import CaracteristicaItem from '../../components/caracteristica/caracteristica-item.jsx'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import GpsFixedIcon from '@mui/icons-material/GpsFixed'
+import foto from './vojtech-bruzek-Yrxr3bsPdS0-unsplash.jpg'
 
 const AlojamientoDetail = () => {
   const { id } = useParams()
@@ -11,66 +14,75 @@ const AlojamientoDetail = () => {
   if (!alojamiento) return <div> `{} Alojamiento no encontrado😔`</div>
 
   return (
-    <div classname="">
-      <div>
-        <h1>{alojamiento.nombre}</h1>
-        <p>
+    <div className="contenedor-general">
+      <div className="contenedor-direccion">
+        <div className="contenedor-titulo">
+          <h1>{alojamiento.nombre}</h1>
+        </div>
+        <p className="especificacion-direccion">
+          <LocationOnIcon fontSize="small" className="location-icon" />
           {alojamiento.direccion.calle} {alojamiento.direccion.numero},{' '}
           {alojamiento.direccion.ciudad} {alojamiento.direccion.pais}
         </p>
-        <p>
+        <p className="locacion-puntual">
+          <GpsFixedIcon fontSize="small" className="gps-icon" />
           Latitud: {alojamiento.direccion.lat} Longitud: {alojamiento.direccion.long}
         </p>
       </div>
       <section className="imagenes">
+        {/* aca pongo una fotarda para ver como va quedando el 
+        diseño */}
+        <img src={foto} alt="" className="foto-hotel" />
         {/* Carousel */}
         {/* <img src={alojamiento.fotos} alt={alojamiento.nombre} /> */}
       </section>
+      <div className="contendor-inferior-foto">
+        <section className="detalles-alojamiento">
+          <div className="descripcion">
+            <h2 className="titulo-descripcion">Descripcion alojamiento</h2>
+            <p>{alojamiento.descripcion}</p>
+          </div>
 
-      <section className="detalles-alojamiento">
-        <div className="descripcion">
-          <h2>Descripcion alojamiento</h2>
-          <p>{alojamiento.descripcion}</p>
-        </div>
+          <section className="caracteristicas">
+            <h1 className="titulo-caracteristicas">¿Que ofrece este lugar?</h1>
+            {alojamiento.caracteristicas.map((caracteristica) => (
+              <CaracteristicaItem caracteristica={caracteristica} />
+            ))}
 
-        <section className="caracteristicas">
-          {alojamiento.caracteristicas.map((caracteristica) => (
-            <CaracteristicaItem caracteristica={caracteristica} />
-          ))}
-
-          {/*
+            {/*
             1. En algun lugar tengo los 4 componentes de caracteristicas
             2. Recorro las caracteristicas y las transformo en uno de esos componentes
             3. Funcion intermedia que toma la caracteristica y devuelve el componente
             4. Cada caracteristica tiene un nombre y un icono
             */}
+          </section>
+
+          <div className="anfitrion">
+            <div className="foto-perfil">
+              <img></img>
+            </div>
+            <div className="informacion-perfil">
+              <p>{alojamiento.anfitrion.nombre}</p>
+              <p>{alojamiento.anfitrion.email}</p>
+            </div>
+          </div>
         </section>
 
-        <div className="anfitrion">
-          <div class="foto-perfil">
-            <img></img>
-          </div>
-          <div class="informacion-perfil">
-            <p>{alojamiento.anfitrion.nombre}</p>
-            <p>{alojamiento.anfitrion.email}</p>
-          </div>
-        </div>
-      </section>
-
-      <section classname="reservar">
-        <div>
-          <Card>
-            <header>
-              `${alojamiento.precioPorNoche} {alojamiento.moneda} por noche`
-            </header>
-            <div classname="contenedor-reservar">
-              <div classname="contenedor-fechas"></div>
-              <div classname="contenedor-viajeros"></div>
+        <section className="contenedor-reserva">
+          <header>
+            `${alojamiento.precioPorNoche} {alojamiento.moneda} por noche`
+          </header>
+          <div className="contenedor-datepicker">
+            <div className="contenedor-fechas">
+              <button></button>
             </div>
-            <button className="boton-reservar">reservar</button>
-          </Card>
-        </div>
-      </section>
+            <div className="contenedor-viajeros">
+              <button></button>
+            </div>
+          </div>
+          <button className="boton-reservar">reservar</button>
+        </section>
+      </div>
     </div>
   )
 }
