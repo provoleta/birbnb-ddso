ab9dde1e1d995d3e532e148e60bc06ae0d3bab5b
Nicolas <naguero@frba.utn.edu.ar>
2025-06-16 12:51:07 -0300

    agrego cosas de css detail-page
    Co-authored-by: Thiago Perez Giacchetta <zBlooDy@users.noreply.github.com>

--- client/birbnb/src/features/detail-page/detail-page.jsx
+++ client/birbnb/src/features/detail-page/detail-page.jsx
@@ -6,6 +6,7 @@ import CaracteristicaItem from '../../components/caracteristica/caracteristica-i
 import LocationOnIcon from '@mui/icons-material/LocationOn'
 import GpsFixedIcon from '@mui/icons-material/GpsFixed'
 import foto from './vojtech-bruzek-Yrxr3bsPdS0-unsplash.jpg'
+import avatar from './avatar-default.png'
 
 const AlojamientoDetail = () => {
   const { id } = useParams()
@@ -15,19 +16,21 @@ const AlojamientoDetail = () => {
 
   return (
     <div className="contenedor-general">
-      <div className="contenedor-direccion">
-        <div className="contenedor-titulo">
-          <h1>{alojamiento.nombre}</h1>
+      <div className="contenedor-fachero">
+        <div className="contenedor-direccion">
+          <div className="contenedor-titulo">
+            <h1>{alojamiento.nombre}</h1>
+          </div>
+          <p className="especificacion-direccion">
+            <LocationOnIcon fontSize="small" className="location-icon" />
+            {alojamiento.direccion.calle} {alojamiento.direccion.numero},{' '}
+            {alojamiento.direccion.ciudad} {alojamiento.direccion.pais}
+          </p>
+          <p className="locacion-puntual">
+            <GpsFixedIcon fontSize="small" className="gps-icon" />
+            Latitud: {alojamiento.direccion.lat} Longitud: {alojamiento.direccion.long}
+          </p>
         </div>
-        <p className="especificacion-direccion">
-          <LocationOnIcon fontSize="small" className="location-icon" />
-          {alojamiento.direccion.calle} {alojamiento.direccion.numero},{' '}
-          {alojamiento.direccion.ciudad} {alojamiento.direccion.pais}
-        </p>
-        <p className="locacion-puntual">
-          <GpsFixedIcon fontSize="small" className="gps-icon" />
-          Latitud: {alojamiento.direccion.lat} Longitud: {alojamiento.direccion.long}
-        </p>
       </div>
       <section className="imagenes">
         {/* aca pongo una fotarda para ver como va quedando el 
@@ -36,52 +39,59 @@ const AlojamientoDetail = () => {
         {/* Carousel */}
         {/* <img src={alojamiento.fotos} alt={alojamiento.nombre} /> */}
       </section>
-      <div className="contendor-inferior-foto">
-        <section className="detalles-alojamiento">
-          <div className="descripcion">
-            <h2 className="titulo-descripcion">Descripcion alojamiento</h2>
-            <p>{alojamiento.descripcion}</p>
-          </div>
-
-          <section className="caracteristicas">
+      <div className="contenedor-facheroinferior">
+        <div className="contendor-inferior-foto">
+          <section className="detalles-alojamiento">
+            <div className="descripcion">
+              <h2 className="titulo-descripcion">Descripcion alojamiento</h2>
+              <p>{alojamiento.descripcion}</p>
+            </div>
             <h1 className="titulo-caracteristicas">¿Que ofrece este lugar?</h1>
-            {alojamiento.caracteristicas.map((caracteristica) => (
-              <CaracteristicaItem caracteristica={caracteristica} />
-            ))}
+            <section className="caracteristicas">
+              {alojamiento.caracteristicas.map((caracteristica) => (
+                <CaracteristicaItem caracteristica={caracteristica} />
+              ))}
 
-            {/*
+              {/*
             1. En algun lugar tengo los 4 componentes de caracteristicas
             2. Recorro las caracteristicas y las transformo en uno de esos componentes
             3. Funcion intermedia que toma la caracteristica y devuelve el componente
             4. Cada caracteristica tiene un nombre y un icono
             */}
+            </section>
+
+            <div className="anfitrion">
+              <h2>Anfitrion</h2>
+              <div className="perfil">
+                <img
+                  className="avatar"
+                  src="/images/avatar-default.png"
+                  alt="Foto de perfil"
+                />
+
+                <div className="informacion-perfil">
+                  <h4>{alojamiento.anfitrion.nombre}</h4>
+                  <p className="email">Contacto: {alojamiento.anfitrion.email}</p>
+                </div>
+              </div>
+            </div>
           </section>
 
-          <div className="anfitrion">
-            <div className="foto-perfil">
-              <img></img>
+          <section className="contenedor-reserva">
+            <header>
+              `${alojamiento.precioPorNoche} {alojamiento.moneda} por noche`
+            </header>
+            <div className="contenedor-datepicker">
+              <div className="contenedor-fechas">
+                <button></button>
+              </div>
+              <div className="contenedor-viajeros">
+                <button></button>
+              </div>
             </div>
-            <div className="informacion-perfil">
-              <p>{alojamiento.anfitrion.nombre}</p>
-              <p>{alojamiento.anfitrion.email}</p>
-            </div>
-          </div>
-        </section>
-
-        <section className="contenedor-reserva">
-          <header>
-            `${alojamiento.precioPorNoche} {alojamiento.moneda} por noche`
-          </header>
-          <div className="contenedor-datepicker">
-            <div className="contenedor-fechas">
-              <button></button>
-            </div>
-            <div className="contenedor-viajeros">
-              <button></button>
-            </div>
-          </div>
-          <button className="boton-reservar">reservar</button>
-        </section>
+            <button className="boton-reservar">Reservar</button>
+          </section>
+        </div>
       </div>
     </div>
   )
