0f8b9219743f1e957e6fac150ca4f3009c39f89e
LucasArias <lucaarias@frba.utn.edu.ar>
2025-06-17 16:04:17 -0300

    hot fix

--- client/birbnb/src/store/search-context.jsx
+++ client/birbnb/src/store/search-context.jsx
@@ -12,6 +12,13 @@ export function SearchProvider({ children }) {
 
   const search = () => {
     let filtrosJson = Object.fromEntries(searchParams)
+    if (filtrosJson.checkIn) {
+      filtrosJson.checkIn = convertirFecha(filtrosJson.checkIn)
+    }
+    if (filtrosJson.checkOut) {
+      filtrosJson.checkOut = convertirFecha(filtrosJson.checkOut)
+    }
+
     console.log(filtrosJson)
     axios
       .get('/alojamientos', {
@@ -30,7 +37,8 @@ export function SearchProvider({ children }) {
 
   const aplicarFiltros = (filtros) => {
     filtros.forEach((filtro, nombre) => {
-      setSearchParams((prev) => prev.set(nombre, filtro))
+      const valor = filtro instanceof Date ? convertirFecha(filtro) : filtro // Convertir fechas al formato DD-MM-YYYY
+      setSearchParams((prev) => prev.set(nombre, valor))
     })
     search()
   }
@@ -38,4 +46,14 @@ export function SearchProvider({ children }) {
   return (
     <SearchContext.Provider value={{ aplicarFiltros }}>{children}</SearchContext.Provider>
   )
+}
+
+const convertirFecha = (fecha) => {
+  if (!(fecha instanceof Date)) {
+    fecha = new Date(fecha) // Asegúrate de que sea un objeto Date
+  }
+  const dia = String(fecha.getDate()).padStart(2, '0') // Agregar ceros si es necesario
+  const mes = String(fecha.getMonth() + 1).padStart(2, '0') // Los meses empiezan en 0
+  const anio = fecha.getFullYear()
+  return `${dia}-${mes}-${anio}`
 }
