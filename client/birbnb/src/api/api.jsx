46c27096f6c578dc128bb4834b62027982503edc
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2025-06-18 20:23:56 -0300

    Agrego manejo de errores en loginpage

--- client/birbnb/src/api/api.jsx
+++ client/birbnb/src/api/api.jsx
@@ -21,7 +21,7 @@ function Api() {
         })
         .catch((error) => {
           console.error('Login failed:', error)
-          alert('Login failed. Please check your credentials and try again.')
+          throw error
         })
       return tokenUsuario
     },
