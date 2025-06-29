7a701a54720b765c0fe44dd7582e08a083346969
Nicolas <naguero@frba.utn.edu.ar>
2025-06-29 15:52:55 -0300

    footer un poco mas lindo pero hay que hacerlo responsive

--- client/birbnb/src/components/footer/footer.jsx
+++ client/birbnb/src/components/footer/footer.jsx
@@ -6,12 +6,34 @@ import './footer.css'
 const Footer = () => {
   return (
     <footer className="pie">
-      <p>© 2025 Birbnb, Inc.</p>
-      <icon className="iconos">
-        <InstagramIcon className="ig" />
-        <TwitterIcon className="tw" />
-        <WhatsAppIcon className="wpp" />
-      </icon>
+      <div className="contenedor-footer">
+        <div className="info-extra">
+          <div className="contenedor-info-izquierda">
+            <p>Información extra</p>
+            <p>Política de privacidad</p>
+            <p>Términos y condiciones</p>
+            <p>Ayuda</p>
+          </div>
+          <div className="contenedor-info-centro">
+            <p>Contacto</p>
+            <p>Soporte técnico</p>
+            <p>Atención al cliente</p>
+          </div>
+          <div className="contenedor-info-derecha">
+            <p>Sobre nosotros</p>
+            <p>Historia de Birbnb</p>
+            <p>Equipo</p>
+          </div>
+        </div>
+        <div className="contenedor-angosto">
+          <p>© 2025 Birbnb, Inc.</p>
+          <icon className="iconos">
+            <InstagramIcon className="ig" />
+            <TwitterIcon className="tw" />
+            <WhatsAppIcon className="wpp" />
+          </icon>
+        </div>
+      </div>
     </footer>
   )
 }
