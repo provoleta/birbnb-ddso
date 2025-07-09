6bdd21f5cbe9efaae7550f1231e0811ee5fb93ba
LucasArias <lucaarias@frba.utn.edu.ar>
2025-07-09 20:01:49 -0300

    Fix formateo

--- client/birbnb/src/features/perfil/components/notification-card/notification-card.jsx
+++ client/birbnb/src/features/perfil/components/notification-card/notification-card.jsx
@@ -15,7 +15,8 @@ const NotificationCard = ({
   handlerMarcarLeida,
 }) => {
   const formatDate = (dateString) => {
-    const date = new Date(dateString)
+    const fechaStr = dateString.split('T')[0]
+    const date = new Date(fechaStr + 'T00:00:00')
     const day = String(date.getDate()).padStart(2, '0')
     const month = String(date.getMonth() + 1).padStart(2, '0')
     const year = date.getFullYear()
